6b1534521f5b82ea4122b94417fd260c1572b68b
hrithikhack007 <hrithik.rs.gupta@gmail.com>
2022-04-07 03:02:42 +0530

    Order Api
    implemented order controllers like creating new order,get details of single order,get orderDetails of logged in user,getAllOrders(--Admin),updateOrderStatus(--Admin),deleteOrder(--Admin)

---
+++ backend/models/orderModel.js
@@ -0,0 +1,111 @@
+const mongoose = require("mongoose");
+const Product = require("./productModel");
+
+const orderSchema = new mongoose.Schema({
+  shippingInfo: {
+    address: {
+      type: String,
+      require: true,
+    },
+    state: {
+      type: String,
+      require: true,
+    },
+    country: {
+      type: String,
+      require: true,
+    },
+
+    pinCode: {
+      type: Number,
+      required: true,
+    },
+
+    phoneNo: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  orderItems: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      image: {
+        type: String,
+        require: true,
+      },
+      product: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Product",
+        require: true,
+      },
+    },
+  ],
+
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    require: true,
+  },
+
+  paymentInfo: {
+    id: {
+      type: String,
+      require: true,
+    },
+    status: {
+      type: String,
+      require: true,
+    },
+  },
+
+  paidAt: {
+    type: Date,
+    required: true,
+  },
+  itemsPrice: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+  taxPrice: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+  shippingPrice: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+
+  totalPrice: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+
+  orderStatus: {
+    type: String,
+    required: true,
+    default: "processing",
+  },
+  deliveredDate: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Order", orderSchema);
